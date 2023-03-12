711ec8b805dec11d0c2c882c8e460baf6502f422
vibrant_coder <74645353+Ernest99@users.noreply.github.com>
2023-03-12 12:46:50 +0000

    modifying some codes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './profile.css'
 import myImg from '../assets/images/me.jpeg'
-import CV from '../assets/images/Ernest-Ghansah-MY-CV.pdf'
 import {HiDownload} from 'react-icons/hi'
 // import { data } from '../data'
 
@@ -14,9 +13,9 @@ const Profile = () => {
                <img src={myImg} alt="" />
                </div>
                <h4>Hi, I'm Ghansah Ernest 🖐️</h4>
-               <h1>Creating Stunning UI/UX Designs, <br /> brands, and Web Applications.</h1>
+               <h1>I Build And Design Stunning, <br /> brands, and Web Interfaces.</h1>
                <div className="cv_btn">
-            <a href="../assets/images/Ernest-Ghansah-MY-CV.pdf" download={CV}>Download CV <span><HiDownload/></span></a>
+            <a href="../assets/images/Ernest-Ghansah-MY-CV.pdf" download='../assets/images/Ernest-Ghansah-MY-CV.pdf'>Download CV <span><HiDownload/></span></a>
         </div>
             </div>
         </div>
